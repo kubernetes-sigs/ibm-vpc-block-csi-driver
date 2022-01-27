68db2f139c5614859402ebaba8f254716c570671
Arashad Ahamad <42564459+arahamad@users.noreply.github.com>
2022-01-27 21:42:59 +0530

    updated vpc volume lib for volume expansion error message (#62)
    * updated vpc volume lib
    
    * updated vpc volume lib

--- vendor/github.com/IBM/ibmcloud-volume-vpc/block/provider/expand_volume.go
+++ vendor/github.com/IBM/ibmcloud-volume-vpc/block/provider/expand_volume.go
@@ -65,7 +65,7 @@ func (vpcs *VPCSession) ExpandVolume(expandVolumeRequest provider.ExpandVolumeRe
 
 	if err != nil {
 		vpcs.Logger.Debug("Failed to expand volume from VPC provider", zap.Reflect("BackendError", err))
-		return -1, userError.GetUserError("FailedToExpandVolume", err)
+		return -1, userError.GetUserError("FailedToExpandVolume", err, volume.ID)
 	}
 
 	vpcs.Logger.Info("Successfully accepted volume expansion request, now waiting for volume state equal to available")
