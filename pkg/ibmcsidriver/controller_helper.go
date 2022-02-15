11716e6fd47f7d22e7acad4ef537ecd0eea3b44f
Ambika Nair <ambiknai@in.ibm.com>
2022-02-15 16:36:02 +0530

    Code changes for SC parameters - zone and region (#67)
    * Changes to accomodate region in sc
    
    * changes for region sc parameter
    
    * cleanup comment
    
    * Review comments
    
    * Review comments

--- pkg/ibmcsidriver/controller_helper.go
+++ pkg/ibmcsidriver/controller_helper.go
@@ -251,16 +251,26 @@ func getVolumeParameters(logger *zap.Logger, req *csi.CreateVolumeRequest, confi
 		volume.Iops = nil
 	}
 
-	//If the zone is not provided in storage class parameters then we pick from the Topology
-	if len(strings.TrimSpace(volume.Az)) == 0 {
+	//if zone is not given in SC parameters, but region is given, error out
+	if len(strings.TrimSpace(volume.Az)) == 0 && len(strings.TrimSpace(volume.Region)) != 0 {
+		err = fmt.Errorf("zone parameter is empty in storage class for region %s", strings.TrimSpace(volume.Region))
+		return volume, err
+	}
+
+	//If both zone and region not provided in storage class parameters then we pick from the Topology
+	//if zone is provided but region is not provided, fetch region for specified zone
+	if len(strings.TrimSpace(volume.Region)) == 0 {
 		zones, err := pickTargetTopologyParams(req.GetAccessibilityRequirements())
 		if err != nil {
-			err = fmt.Errorf("unable to fetch zone information: '%v'", err)
+			err = fmt.Errorf("unable to fetch zone information from topology: '%v'", err)
 			logger.Error("getVolumeParameters", zap.NamedError("InvalidParameter", err))
 			return volume, err
 		}
 		volume.Region = zones[utils.NodeRegionLabel]
-		volume.Az = zones[utils.NodeZoneLabel]
+		if len(strings.TrimSpace(volume.Az)) == 0 {
+			volume.Az = zones[utils.NodeZoneLabel]
+		}
+
 	}
 
 	return volume, nil
