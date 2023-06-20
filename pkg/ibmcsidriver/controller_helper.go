5b2d8fbf15675e6004adf1b97aa6a47c12562a5d
Sameer Shaikh <sameer.shaikh@ibm.com>
2023-06-21 01:53:26 +0530

    Introduce Snapshot feature flag to disable/enable on demand

--- pkg/ibmcsidriver/controller_helper.go
+++ pkg/ibmcsidriver/controller_helper.go
@@ -513,8 +513,9 @@ func getPrefedTopologyParams(topList []*csi.Topology) (map[string]string, error)
 func getMaxDelaySnapshotCreate(ctxLogger *zap.Logger) int {
 	maxDelaySnapshotCreate := MAX_DELAY_SNAPSHOT_CREATE // 300 seconds default
 	maxDelayEnv := os.Getenv("MAX_DELAY_SNAPSHOT_CREATE")
+	var err error
 	if maxDelayEnv != "" {
-		maxDelaySnapshotCreate, err := strconv.Atoi(maxDelayEnv) //nolint
+		maxDelaySnapshotCreate, err = strconv.Atoi(maxDelayEnv)
 		if err != nil {
 			maxDelaySnapshotCreate = MAX_DELAY_SNAPSHOT_CREATE // 300 seconds default
 			ctxLogger.Warn("Error while processing MAX_DELAY_SNAPSHOT_CREATE variable. MAX_DELAY_SNAPSHOT_CREATE expects integer value in seconds, continuing with default value 300s", zap.Any("MAX_DELAY_SNAPSHOT_CREATE", maxDelayEnv), zap.Any("Considered value", maxDelaySnapshotCreate), zap.Error(err))
