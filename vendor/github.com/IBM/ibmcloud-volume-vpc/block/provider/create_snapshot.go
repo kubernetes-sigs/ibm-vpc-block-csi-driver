ee82aaa419a018544138c18962c381e9ed97a247
Mahantesh-R <mahantesh.r@ibm.com>
2026-02-09 20:48:33 +0530

    vpc module changes pull
    Signed-off-by: Mahantesh-R <mahantesh.r@ibm.com>

--- vendor/github.com/IBM/ibmcloud-volume-vpc/block/provider/create_snapshot.go
+++ vendor/github.com/IBM/ibmcloud-volume-vpc/block/provider/create_snapshot.go
@@ -31,23 +31,38 @@ import (
 const snapshotReadyState = "stable"
 
 // CreateSnapshot creates snapshot
-func (vpcs *VPCSession) CreateSnapshot(sourceVolumeID string, snapshotParameters provider.SnapshotParameters) (*provider.Snapshot, error) {
-	vpcs.Logger.Info("Entry CreateSnapshot", zap.Reflect("snapshotRequest", snapshotParameters), zap.Reflect("sourceVolumeID", sourceVolumeID))
-	defer vpcs.Logger.Info("Exit CreateSnapshot", zap.Reflect("snapshotRequest", snapshotParameters), zap.Reflect("sourceVolumeID", sourceVolumeID))
+func (vpcs *VPCSession) CreateSnapshot(sourceVolumeID string, snapshotMetadata provider.SnapshotParameters, snapshotClassParams map[string]string) (*provider.Snapshot, error) {
+	vpcs.Logger.Info("Entry CreateSnapshot", zap.Reflect("snapshotRequest", snapshotMetadata), zap.Reflect("sourceVolumeID", sourceVolumeID))
+	defer vpcs.Logger.Info("Exit CreateSnapshot", zap.Reflect("snapshotRequest", snapshotMetadata), zap.Reflect("sourceVolumeID", sourceVolumeID))
 	defer metrics.UpdateDurationFromStart(vpcs.Logger, "CreateSnapshot", time.Now())
-	var err error
-	err = vpcs.validateSnapshotRequest(sourceVolumeID)
+
+	err := vpcs.validateSnapshotRequest(sourceVolumeID)
 	if err != nil {
 		return nil, err
 	}
 	var snapshotResult *models.Snapshot
 
-	// Step 1- validate input which are required
+	// cluster's resource group
+	snapshotRG := vpcs.Config.VPCConfig.G2ResourceGroupID
+
+	// volumeSnapshotClass defined resource group
+	if rg, ok := snapshotClassParams["resourceGroup"]; ok && rg != "" {
+		snapshotRG = rg
+
+		// TODO: add cluster validation condition
+		vpcs.Logger.Info("Using snapshot RG from VolumeSnapshotClass", zap.String("snapshotRG", snapshotRG))
+	}
+
+	vpcs.Logger.Info("Final snapshot RG selected", zap.String("snapshotRG", snapshotRG))
 
 	snapshotTemplate := &models.Snapshot{
-		Name:          snapshotParameters.Name,
-		SourceVolume:  &models.SourceVolume{ID: sourceVolumeID},
-		ResourceGroup: &models.ResourceGroup{ID: vpcs.Config.VPCConfig.G2ResourceGroupID},
+		Name: snapshotMetadata.Name,
+		SourceVolume: &models.SourceVolume{
+			ID: sourceVolumeID,
+		},
+		ResourceGroup: &models.ResourceGroup{
+			ID: snapshotRG,
+		},
 	}
 
 	err = retry(vpcs.Logger, func() error {
@@ -59,8 +74,10 @@ func (vpcs *VPCSession) CreateSnapshot(sourceVolumeID string, snapshotParameters
 	}
 
 	vpcs.Logger.Info("Successfully created snapshot with backend (vpcclient) call. Snapshot details", zap.Reflect("Snapshot", snapshotResult))
+
 	// Converting volume to lib snapshot type
 	snapshotResponse := FromProviderToLibSnapshot(snapshotResult, vpcs.Logger)
+
 	vpcs.Logger.Info("SnapshotResponse", zap.Reflect("snapshotResponse", snapshotResponse))
 	return snapshotResponse, err
 }
